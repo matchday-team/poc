4c7daaea0fc2aa99234889f96dfec16932daed34
roky <rookedsysc36@gmail.com>
2025-05-02 16:28:22 +0900

    modify: api spec 변경 대응 및 logging 개선

--- web-socket-poc/scripts/setup_match.py
+++ web-socket-poc/scripts/setup_match.py
@@ -1,3 +1,5 @@
+import uuid
+
 import requests
 
 BASE_URL = "http://localhost:8080/api/v1"
@@ -7,7 +9,7 @@ def create_user(name):
     url = f"{BASE_URL}/users"
     payload = {"name": name}
     response = requests.post(url, json=payload)
-    print(f"Created user {name}: {response.json()}")
+    print(f"Created user {response.json()['data']}: {response.json()}")
     return response.json()["data"]
 
 
@@ -23,7 +25,12 @@ def create_team(name, team_color):
         _type_: _description_
     """
     url = f"{BASE_URL}/teams"
-    payload = {"name": name, "teamColor": team_color}
+    payload = {
+        "name": name,
+        "teamColor": team_color,
+        "bottomColor": team_color,
+        "stockingColor": team_color,
+    }
     response = requests.post(url, json=payload)
     print(f"Created team {name}: {response.json()}")
     return response.json()["data"]
@@ -33,7 +40,7 @@ def join_team(user_id, team_id, number, position):
     url = f"{BASE_URL}/users/{user_id}/teams"
     payload = {"teamId": team_id, "number": number, "defaultPosition": position}
     response = requests.post(url, json=payload)
-    print(f"User {user_id} joined team {team_id}: {response.json()}")
+    print(f"User {user_id}  {team_id}: {response.json()}")
     return response.json()["data"]
 
 
@@ -51,13 +58,14 @@ def create_match(title, home_team_id, away_team_id):
     }
     response = requests.post(url, json=payload)
     print(f"Created match {title}: {response.json()}")
-    return 1  # Assuming match ID is 1
+    return response.json()["data"]
 
 
-def register_match_user(match_id, user_id, role):
+def register_match_user(match_id, user_id, team_id, role):
     url = f"{BASE_URL}/matches/{match_id}/users"
     payload = {
         "userId": user_id,
+        "teamId": team_id,
         "role": role,
         "matchPosition": "FW",
         "matchGrid": "A1",
@@ -68,25 +76,27 @@ def register_match_user(match_id, user_id, role):
 
 def main():
     # Create users
-    hong_id = create_user("국길동")
-    jang_id = create_user("킹길동")
-    kang_id = create_user("왕길동")
+    hong_id = create_user(str(uuid.uuid4())[:3])
+    jang_id = create_user(str(uuid.uuid4())[:3])
+    kang_id = create_user(str(uuid.uuid4())[:3])
 
     # Create teams
-    real_id = create_team("레알", "#FFFFFF")
-    tottenham_id = create_team("토트넘", "#000000")
+    team1_name = str(uuid.uuid4())
+    team2_name = str(uuid.uuid4())
+    team1_id = create_team(team1_name, "#FFFFFF")
+    team2_id = create_team(team2_name, "#000000")
 
     # Join teams
-    join_team(hong_id, real_id, 7, "FW")
-    join_team(jang_id, tottenham_id, 7, "FW")
+    join_team(hong_id, team1_id, 7, "FW")
+    join_team(jang_id, team2_id, 7, "FW")
 
     # Create match
-    match_id = create_match("레알 vs 토트넘", real_id, tottenham_id)
+    match_id = create_match(team1_name + " vs " + team2_name, team1_id, team2_id)
 
     # Register match users
-    register_match_user(match_id, hong_id, "START_PLAYER")
-    register_match_user(match_id, jang_id, "START_PLAYER")
-    register_match_user(match_id, kang_id, "ARCHIVES")
+    register_match_user(match_id, hong_id, team1_id, "START_PLAYER")
+    register_match_user(match_id, jang_id, team2_id, "START_PLAYER")
+    register_match_user(match_id, kang_id, None, "ARCHIVES")
 
 
 if __name__ == "__main__":
